43f280ab733f56b039578cb51ad335c4789b1fb8
Jonas Leininger <jonas.leininger@gmail.com>
2019-06-03 11:18:13 +0200

    implement double dqn

---
+++ pytorch/double_dqn_agent.py
@@ -0,0 +1,96 @@
+import os
+import numpy as np
+import torch
+import torch.nn.functional as F
+
+from pytorch.dqn import DQN
+from pytorch.replay_memory import ReplayMemory
+
+
+class Agent():
+
+    def __init__(self, state_size: int, action_size: int):
+        self.buffer_size = int(1e5)
+        self.batch_size = 64
+        self.tau = 1e-3
+        self.update_every = 4
+        self.state_size = state_size
+        self.action_size = action_size
+        self.gamma = 0.99
+        self.epsilon = 1.0
+        self.epsilon_min = 0.01
+        self.epsilon_decay = 0.995
+        self.learning_rate = 0.0005
+        self.checkpoint_path = "checkpoints/pytorch/cp-{epoch:04d}.pt"
+        
+        self.device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+        self.qnetwork = DQN(self.state_size, self.action_size)
+        self.qnetwork.to(self.device)
+        self.optimizer = torch.optim.Adam(self.qnetwork.parameters(), lr=self.learning_rate)
+
+        self.tragetnetwork = DQN(self.state_size, self.action_size)
+        self.tragetnetwork.to(self.device)
+        self.tragetnetwork.load_state_dict(self.qnetwork.state_dict())
+        
+        self.memory = ReplayMemory(self.action_size, self.buffer_size, self.batch_size)
+    
+    def step(self, state, action, reward, next_state, done):
+        '''
+        Save experience in replay memory
+        
+        Params
+        :param state: state
+        :param action: action
+        :param reward: reward
+        :param next_state: next_staet
+        :param done: done
+        :return:
+        '''
+        self.memory.add(state, action, reward, next_state, done)
+    
+    def act(self, state):
+        '''
+        Agents choosen action for given state
+        
+        Params
+        :param state: state to act on
+        :return: action
+        '''
+        state = torch.from_numpy(state).float().unsqueeze(0).to(self.device)
+
+        if np.random.rand() <= self.epsilon:
+            return np.random.randint(0, self.action_size)
+        act_values = self.qnetwork(state)
+        return np.argmax(act_values.cpu().data.numpy())
+    
+    def replay(self):
+        '''
+        Train agent on the replay memory. DQN algorithm with local q-network and target-network
+
+        Params
+        :return: None
+        '''
+        states, actions, rewards, next_states, dones = self.memory.sample()
+        nextstate_action_index = self.qnetwork(next_states).max(dim=1)[1].unsqueeze(1)
+        targetnetwork_outputs = self.tragetnetwork(next_states).gather(1, nextstate_action_index)
+        targets = rewards + (self.gamma * targetnetwork_outputs)*(1 - dones)
+        expected = self.qnetwork(states).gather(1, actions)
+        predicts = self.qnetwork(states)
+        
+        self.loss = F.mse_loss(expected, targets)
+        self.optimizer.zero_grad()
+        self.loss.backward()
+        self.optimizer.step()
+
+        self.update_target_network()
+    
+    def update_target_network(self):
+        self.tragetnetwork.load_state_dict(self.qnetwork.state_dict())
+    
+    def save_checkpoint(self, epoch: int):
+        torch.save({
+            'epoch': epoch,
+            'model_state_dict': self.qnetwork.state_dict(),
+            'optimizer_state_dict': self.optimizer.state_dict(),
+            'loss': self.loss
+        }, self.checkpoint_path.format(epoch=epoch))
---
+++ run_doubledqn.py
@@ -0,0 +1,88 @@
+import time
+import numpy as np
+from collections import deque
+
+from unityagents import UnityEnvironment
+
+from pytorch.double_dqn_agent import Agent
+
+def main():
+    env = UnityEnvironment(file_name="Banana_Linux/Banana.x86_64")
+    brain_name = env.brain_names[0]
+    brain = env.brains[brain_name]
+    env_info = env.reset(train_mode=True)[brain_name]
+    print('Number of agents:', len(env_info.agents))
+    action_size = brain.vector_action_space_size
+    print('Number of actions:', action_size)
+    state = env_info.vector_observations[0]
+    print('States look like:', state)
+    state_size = len(state)
+    print('States have length:', state_size)
+
+    # env_info = env.reset(train_mode=False)[brain_name]
+    # state = env_info.vector_observations[0]
+    # action_size = brain.vector_action_space_size
+    # state_size = len(state)
+    # score = 0 
+    # while True:
+    #     action = np.random.randint(action_size)
+    #     env_info = env.step(action)[brain_name]
+    #     next_state = env_info.vector_observations[0]
+    #     reward = env_info.rewards[0]
+    #     done = env_info.local_done[0]
+    #     score += reward
+    #     state = next_state
+    #     time.sleep(0.0001)
+    #     if done:
+    #         break
+        
+    # print("Score: {}".format(score))
+
+    env_info = env.reset(train_mode=True)[brain_name]
+    state = env_info.vector_observations[0]
+    action_size = brain.vector_action_space_size
+    state_size = len(state)
+    agent = Agent(state_size=state_size, action_size=action_size)
+    episodes = 5000
+    scores_window = deque(maxlen=100)
+    scores = []
+    for e in range(episodes + 1):
+        env_info = env.reset(train_mode=True)[brain_name]
+        state = env_info.vector_observations[0]
+        state = np.reshape(state, [1, state_size])
+        score = 0
+        
+        for t in range(500):
+            action = agent.act(state)
+            env_info = env.step(action)[brain_name]
+            next_state = env_info.vector_observations[0]
+            next_state = np.reshape(next_state, [1, state_size])
+            reward = env_info.rewards[0]
+            done = env_info.local_done[0]
+            agent.step(state, action, reward, next_state, done)
+            state = next_state
+            score += reward
+            if done:
+                print("episode: {}/{}, score: {}, after time: {}".format(e, episodes, score, t))
+                break
+                
+            if ((t+1)% 4) == 0:
+                if agent.memory.__len__()>=64:
+                    agent.replay()
+            
+        if agent.epsilon > agent.epsilon_min:
+            agent.epsilon *= agent.epsilon_decay
+        
+        scores.append(score)
+        scores_window.append(score)
+        if (e)%100 == 0:
+            mean_score = np.mean(scores_window)
+            agent.save_checkpoint(epoch=e)
+            scores_arr = np.asarray(scores)
+            np.save(file="dqn_saved_scores.npy", arr=scores_arr)
+            print('\rEpisode {}\tAverage Score: {:.2f}'.format(e, mean_score))
+            if mean_score >= 10.0:
+                break
+
+if __name__=='__main__':
+    main()
